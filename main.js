66620f0e0b717b1a2df7ae087c0c98c8ed3d5d5f
Amal-Mansour85 <mansour.amal85@gmail,com>
2020-08-18 01:21:55 +0100

    half finish

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 $(document).ready(function(){	
  
 	$("body").on('click',function(){
-		if($("img").attr("src")==="first for first.jpg"){$("img").attr("src","second for first.jpg")}
-  else if($("img").attr("src")==="second for first.jpg"){$("img").attr("src","crative thinking.png")}
-  	else if($("img").attr("src")===""){$("img").attr("src","first for first.jpg")}
+		if($("img").attr("src")==="image1.jpg"){$("img").attr("src","image2.jpg")}
+  else if($("img").attr("src")==="image2.jpg"){$("img").attr("src","image3.jpg")}
+  	else if($("img").attr("src")===""){$("img").attr("src","image1.jpg")}
 		var showImage=("<div></div>");  
-      //$("img").show(3500);
+      
       $("img").css("visibility", "visible")
 });
 	$("#myButton").on('click',function(){
 		var myClick= ("<div></div>");
 		$("li").css("visibility" , "visible")
-	     $("li").css({"color":"white","font-size": "30px"})
+	 $("li").css({"color":"white","font-size": "30px","font-weight":"bold"})
+	 $("p").css("visibility" , "visible")
+	$("p").css({"color": " #00ccff", "font-size": "150%","text-align":"left" ,"font-weight":"bold"})
 	      
 	    
 	});
